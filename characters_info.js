d48529abac35ea662f5c26d78f89aea4f7afc7e2
sir-angus-mac <angusmak0324@gmail.com>
2026-06-20 12:52:05 +0900

    Initial commit

---
+++ characters_info.js
@@ -0,0 +1,21 @@
+// characters_info.js 
+const CHARACTERS_DATA = [
+    {
+        folderName: "eren",
+        eng_name: "Eren Yeager",
+        jp_name: "エレン・イェーガー",
+        eng_origin: "Paradi Island", 
+        jp_origin: "パラディ 島",
+        eng_text: "Eren spent his youth fighting as a soldier in the Survey Corps to protect the citizens of Paradis Island from man-eating Titans. However, after discovering the world's deep-seated hatred for his people, he unleashed the Rumbling to wipe out his enemies. He ultimately slaughtered 80% of the global human population, though his actions forced a temporary peace that allowed his homeland to prosper for a time.",
+        jp_text: "エレンは人生の多くを調査兵団の兵士として過ごし、人を喰らう巨人からパラディ島の市民を守るために戦いました。しかし、世界が彼の同胞に対して抱く深い憎しみを知った後、彼は敵を滅ぼすために「地鳴らし」を発動させました。最終的に全人類の8割を虐殺したものの、その行為によって世界に一時的な平和がもたらされ、彼の故郷はしばらくの間繁栄することとなりました。"
+    },
+    { 
+        folderName: "lincoln",
+        eng_name: "Abraham Lincoln",
+        jp_name: "エイブラハム・リンカーン",
+        eng_origin: "America",
+        jp_origin: "アメリカ",
+        eng_text: "Abraham Lincoln served as the 16th President of the United States during the American Civil War. He successfully preserved the Union and issued the Emancipation Proclamation, which led to the abolition of slavery in America, securing freedom for millions of enslaved people.",
+        jp_text: "エイブラハム・リンカーンは、南北戦争の激動期に第16代アメリカ合衆国大統領を務めました。彼は国家の分裂を防ぎ（連邦を維持し）、「奴隷解放宣言」を発布することでアメリカにおける奴隷制の廃止を導き、数百万人もの奴隷とされていた人々に自由をもたらしました。"
+    }
+];
